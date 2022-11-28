134d339b94780ae8aab929a7f9d6d4023eb7368a
Juan camilo Villacob <107330476+jcvillacob@users.noreply.github.com>
2022-11-28 21:45:07 +0000

    Comprobado campos tarjetas

--- src/app/components/crear-tarjeta/crear-tarjeta.component.ts
+++ src/app/components/crear-tarjeta/crear-tarjeta.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TarjetaCredito } from 'src/app/models/TarjetaCredito';
 
 @Component({
   selector: 'app-crear-tarjeta',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crear-tarjeta.component.css']
 })
 export class CrearTarjetaComponent implements OnInit {
+  form: FormGroup
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      titular: ["", Validators.required],
+      numeroTarjeta: ["", [Validators.required, Validators.maxLength(16), Validators.minLength(16)]],
+      fechaExpiracion: ["", [Validators.required, Validators.maxLength(5), Validators.minLength(5)]],
+      cvv: ["", [Validators.required, Validators.maxLength(3), Validators.minLength(3)]],
+    })
+   }
 
   ngOnInit(): void {
   }
 
+  crearTarjeta(){
+    const TARJETA: TarjetaCredito = {
+      titular: this.form.value.titular,
+      numeroTarjeta: this.form.value.numeroTarjeta,
+      fechaExpiracion: this.form.value.fechaExpiracion,
+      cvv: this.form.value.cvv,
+      fechaCreacion: new Date(),
+      fechaActualizacion: new Date()
+    }
+    console.log(TARJETA);
+  }
 }
